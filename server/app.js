020a547db2e4f9e1fd27c5c838d6fa99e17d04eb
root <aliriza.dikici@gmail.com>
2016-04-03 06:28:06 +0100

    Regular Commit

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ console.log(lnconfiguration);
 var cloudConnStr	= lnconfiguration.db.connstr;
 var cloudColls		= ['users','datacenters','nodes','ipblocks','storages','nodecs','nodetokens','managers','plans','servers','images', 'imagegroups','isofiles', 'logs', 'userfiles', 'settings', 'invoices', 'counters', 'mailtemplates', 'library', 'templateDocs', 'userRequests', 'transactions'];
 var db 				= mongojs(cloudConnStr, cloudColls, { authMechanism : 'ScramSHA1' });
+console.log(db);
 
 var App             = require('./config/config.app.js');
 var cronerpid 		= 0;
