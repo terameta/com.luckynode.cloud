fb61e38c86e70b4122447343d6c733a7cf77a42e
root <aliriza.dikici@gmail.com>
2016-04-04 05:11:10 +0100

    Regular Commit

--- server/modules/module.server.js
+++ server/modules/module.server.js
@@ -94,7 +94,7 @@ function listServers(query){
 
 function persistentInitiateVNCProxy(host, port, localport, tools){
 	var deferred = Q.defer();
-	var curCommand = path.resolve('.') + '/client/lib/no-vnc/utils/websockify '+localport+' '+ host +':'+ port +' --cert /etc/nginx/ssl/luckynode.pem --ssl-only -D --run-once --timeout=60';
+	var curCommand = path.resolve('.') + '/client/lib/no-vnc/utils/websockify '+localport+' '+ host +':'+ port +' --cert /etc/nginx/ssl/www_epmvirtual_com.crt --key /etc/nginx/ssl/www_epmvirtual_com.key --ssl-only -D --run-once --timeout=60';
 	tools.runLocalCommand(curCommand).then(function(result){
 		deferred.resolve(localport);
 	}).fail(function(issue){
