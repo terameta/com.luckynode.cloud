cc799c5c14c412966ef829eb0afad0313e0f78b2
root <aliriza.dikici@gmail.com>
2016-04-07 12:07:36 +0100

    Regular Commit

---
+++ croner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/node.luckynode.com
+date
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+echo $reslog
+if [ "$reslog" != "" ] ; then
+    echo thereischange
+    git reset --hard origin/master
+    git merge origin/master
+    forever restart cloudluckynode
+else
+    echo nochange
+fi
