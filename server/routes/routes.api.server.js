d86cd2a1bf9603fe0372259180179f1474d94efe
root <aliriza.dikici@gmail.com>
2016-04-20 11:47:01 +0100

    Regular Commit

--- server/routes/routes.api.server.js
+++ server/routes/routes.api.server.js
@@ -391,6 +391,9 @@ module.exports = function(app, express, db, tools) {
 };
 
 function isThisOurServer(refObject){
+	console.log("=============================================================");
+	console.log("=============================================================");
+	console.log("=============================================================");
 	console.log(refObject);
 	var theReq = refObject.req;
 	var ip = theReq.headers['x-forwarded-for'] || theReq.connection.remoteAddress || theReq.socket.remoteAddress || theReq.connection.socket.remoteAddress;
