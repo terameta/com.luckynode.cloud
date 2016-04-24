9cf4505c4b02befca267d815e78617203c8cb64d
root <aliriza.dikici@gmail.com>
2016-04-24 13:04:05 +0100

    Regular Commit

--- server/routes/routes.api.backup.js
+++ server/routes/routes.api.backup.js
@@ -2,43 +2,12 @@ module.exports = function(app, express, db, tools) {
 	var mongojs 		= require('mongojs');
 	var apiRoutes 		= express.Router();
 
-	apiRoutes.get('/getiplist', function(req, res){
-		db.nodes.find({}, function(nerr, ndata){
-			var isSourceValid = false;
-			ndata.forEach(function(curNode){
-				if(req.ip == curNode.ip) {
-					isSourceValid = true;
-				}
-				if(curNode.internalip){
-					if(req.ip == curNode.internalip){
-						isSourceValid = true;
-					}
-				}
-			});
-			if(isSourceValid){
-				db.managers.find({}, function(err, data){
-					if(err){
-						res.status(500).json({ status: "fail" });
-					} else {
-						var toReturn = [];
-						data.forEach(function(curManager){
-							toReturn.push(curManager.ip);
-							if(curManager.internalip) toReturn.push(curManager.internalip);
-						});
-						res.send(toReturn);
-					}
-				});
-			} else {
-				res.status(400).json({ status: "fail"});
-			}
-		});
-	});
-
 	apiRoutes.get('/b2credentials', function(req, res) {
 		db.settings.find({}, function(err, settings){
 			if(err){
 				res.status(500).send(err);
 			} else {
+				console.log(settings);
 				res.send(settings.backblaze.enabledips + ":::"+ req.ip);
 			}
 		});
