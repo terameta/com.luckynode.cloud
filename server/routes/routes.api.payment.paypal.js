9e20755a8e342fa88259a5e9fe2c21fb2896ce12
root <aliriza.dikici@gmail.com>
2016-04-28 12:25:53 +0100

    Regular Commit

--- server/routes/routes.api.payment.paypal.js
+++ server/routes/routes.api.payment.paypal.js
@@ -1,7 +1,7 @@
 var db;
 var Q					= require('q');
 var mongojs 		= require('mongojs');
-var https 			= require('https');
+var request 		= require('request');
 var invoiceModule;
 
 module.exports = function(app, express, refdb, tools) {
@@ -153,6 +153,8 @@ function listPaypal(cObject, listPage){
 	if(!cObject){ deferred.reject({onFunction:"listPaypal", err:"No Object Passed"}); return deferred.promise;}
 	if(!cObject.paypal){ deferred.reject({onFunction:"listPaypal", err:"No Paypal detail passed in the object"}); return deferred.promise;}
 
+
+
 	var data = JSON.stringify({
 		USER:cObject.settings.paypal.username,
 		PWD:cObject.settings.paypal.password,
@@ -163,34 +165,23 @@ function listPaypal(cObject, listPage){
 		VERSION:94
 	});
 
-	var options = {
-		host: 'api-3t.paypal.com',
-		//port: '80',
-		path: '/nvp',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json; charset=utf-8',
-			'Content-Length': data.length
-		}
-	};
-
-	var req = https.request(options, function(res) {
-		var msg = '';
-
-		res.setEncoding('utf8');
-		res.on('data', function(chunk) {
-			msg += chunk;
-		});
-		res.on('end', function() {
-			console.log(JSON.parse(msg));
-			cObject.invoiceList = msg;
+	request.post({url:'https://api-3t.paypal.com/nvp', form: data}, function(err,httpResponse,body){
+		if(err){
+			console.log("Error: ", err);
+			deferred.reject(err);
+		} else {
+			console.log("=======================================================");
+			console.log("=======================================================");
+			console.log(httpResponse);
+			console.log("=======================================================");
+			console.log("=======================================================");
+			console.log(body);
+			console.log("=======================================================");
+			console.log("=======================================================");
+			cObject.invoiceList = body;
 			deferred.resolve(cObject);
-		});
+		}
 	});
-
-	req.write(data);
-	req.end();
-
 
 /*
 	cObject.paypal.payment.list({ "count": 20 }, function(error, payment) {
