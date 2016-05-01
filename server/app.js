e8a852e691a9156bafee94f711556abcc004b5cb
root <aliriza.dikici@gmail.com>
2016-05-01 06:58:34 +0100

    Regular Commit

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var mongojs 		= require('mongojs');
 var lnconfiguration	= JSON.parse(fs.readFileSync('luckynode.conf', 'utf8'));
 //console.log(lnconfiguration);
 var cloudConnStr	= lnconfiguration.db.connstr;
-var cloudColls		= ['users','datacenters','nodes','ipblocks','storages','nodecs','nodetokens','managers','plans','servers','images', 'imagegroups','isofiles', 'logs', 'userfiles', 'settings', 'invoices', 'counters', 'mailtemplates', 'library', 'templateDocs', 'userRequests', 'transactions'];
+var cloudColls		= ['users','datacenters','nodes','ipblocks','storages','nodecs','nodetokens','managers','plans','servers','images', 'imagegroups','isofiles', 'logs', 'userfiles', 'settings', 'invoices', 'counters', 'mailtemplates', 'library', 'templateDocs', 'userRequests', 'transactions', 'countries'];
 var db 				= mongojs(cloudConnStr, cloudColls, { authMechanism : 'ScramSHA1' });
 //db.servers.find(function(err, result){console.log(result);});
 
