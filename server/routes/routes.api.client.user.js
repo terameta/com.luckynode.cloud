aac71c9f8afc83ff0401701a4823668f6b426ef9
root <aliriza.dikici@gmail.com>
2016-05-03 12:52:03 +0100

    Regular Commit

--- server/routes/routes.api.client.user.js
+++ server/routes/routes.api.client.user.js
@@ -11,7 +11,7 @@ module.exports = function(app, express, db, tools) {
 		} else if(!req.user.id){
 			res.status(400).json({ status: "fail", detail: "no user provided" });
 		} else {
-			db.users.findOne({_id:mongojs.ObjectId(req.user.id)},{accountTransactions:0, pass:0, verificationcode:0},function(err, user){
+			db.users.findOne({_id:mongojs.ObjectId(req.user.id)},{accountTransactions:0, pass:0, verificationcode:0, role:0},function(err, user){
 				if(err || !user){
 					res.status(500).json({status:"fail", message:"Failed to receive user details"});
 				} else {
