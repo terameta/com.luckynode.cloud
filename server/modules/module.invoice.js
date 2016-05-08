034421cf28e524ce4d4fe6f1c49009f50c61704a
root <aliriza.dikici@gmail.com>
2016-05-08 09:11:46 +0100

    Regular Commit

--- server/modules/module.invoice.js
+++ server/modules/module.invoice.js
@@ -49,9 +49,9 @@ function informBalance(refObj){
 
 function decideBalance(refObj){
 	if(refObj.accountBalance > 0){
-	//	console.log(refObj.userid, refObj.accountBalance, "We should communicate");
+		console.log(refObj.userid, refObj.accountBalance, "We should communicate");
 	} else {
-	//	console.log(refObj.userid, refObj.accountBalance, "No need to communicate");
+		console.log(refObj.userid, refObj.accountBalance, "No need to communicate");
 	}
 }
 
