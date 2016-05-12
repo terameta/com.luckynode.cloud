5e832b1e95281456c742bec5dcc1d16d4359a3d4
root <aliriza.dikici@gmail.com>
2016-05-12 13:08:55 +0100

    Regular Commit

--- server/tools/tools.automailer.js
+++ server/tools/tools.automailer.js
@@ -4,3 +4,11 @@ var mongojs 		= require('mongojs');
 var Q				= require('q');
 var nodemailer 		= require('nodemailer');
 var smtpTransport 	= require('nodemailer-smtp-transport');
+
+module.exports = function mailerModule(refdb){
+	db = refdb;
+	var module = {
+
+	};
+	return module;
+};
