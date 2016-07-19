6e4ae71bfa37a1e8e057cecc9568fc554f9e99f5
root <aliriza.dikici@gmail.com>
2016-07-19 12:49:21 +0100

    Regular Commit

--- client/admin/js/admin.js
+++ client/admin/js/admin.js
@@ -120,19 +120,3 @@ adminApp.run(['$rootScope', '$state', '$signinModal', '$localStorage', function(
 		}
 	});
 }]);
-
-adminApp.config(['$provide',function($provide) {
-	$provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions){
-        // $delegate is the taOptions we are decorating
-        // register the tool with textAngular
-        taRegisterTool('colourRed', {
-            iconclass: "fa fa-square red",
-            action: function(){
-                this.$editor().wrapSelection('forecolor', 'red');
-            }
-        });
-        // add the button to the default toolbar definition
-        taOptions.toolbar[0].push('colourRed');
-        return taOptions;
-    }]);
-}]);
