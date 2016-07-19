c84d9953d7c78fb267cfe9ec50df694f6c4be141
root <aliriza.dikici@gmail.com>
2016-07-19 12:21:23 +0100

    Regular Commit

--- client/admin/js/admin.js
+++ client/admin/js/admin.js
@@ -122,14 +122,17 @@ adminApp.run(['$rootScope', '$state', '$signinModal', '$localStorage', function(
 }]);
 
 adminApp.config(['$provide',function($provide) {
-	$provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions) {
-		taRegisterTool('insertMyHtml', {
-			buttontext: 'insert my html',
-			action: function(taRegisterTool, taOptions) {
-				this.$editor().wrapSelection('insertHtml', '<h1>Hello, world!</h1>');
-			}
-		});
-		taOptions.toolbar = ['insertMyHtml'];
-		return taOptions;
-	}]);
+	$provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions){
+        // $delegate is the taOptions we are decorating
+        // register the tool with textAngular
+        taRegisterTool('colourRed', {
+            iconclass: "fa fa-square red",
+            action: function(){
+                this.$editor().wrapSelection('forecolor', 'red');
+            }
+        });
+        // add the button to the default toolbar definition
+        taOptions.toolbar[1].push('colourRed');
+        return taOptions;
+    }]);
 }]);
