749758fd9c77ef9d46405814e606d3695740febc
root <aliriza.dikici@gmail.com>
2016-09-07 17:19:35 +0100

    Regular Commit

--- server/config/config.croner.js
+++ server/config/config.croner.js
@@ -28,6 +28,5 @@ module.exports = function Croner(db) {
 	//var job2H = new croner('0 0 */2 * * *',function(){crones.everyotherhour();}, function(){ console.log("This is the end");},true,"America/Los_Angeles");
 	//var jobH = new croner('0 0 * * * *',function(){crones.everyhour();  }, function(){ console.log("This is the end");},true,"America/Los_Angeles");
 	//var jobTM = new croner('30 5,15,25,35,45,55 * * * *',function(){crones.everytenminutes();  }, function(){ console.log("This is the end");},true,"America/Los_Angeles");
-
 	module.exports = Croner;
 };
