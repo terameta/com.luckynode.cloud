0d45feebb0aaaff23b3877d15f4661714dcb54d1
root <aliriza.dikici@gmail.com>
2016-09-07 08:22:09 +0100

    Regular Commit

--- forever.sh
+++ forever.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/cloud.luckynode.com
-forever -a --minUptime 1000 --spinSleepTime 5000 --uid "cloudluckynode" -c "--trace_gc" start server/app.js
+forever -a --minUptime 1000 --spinSleepTime 5000 --uid "cloudluckynode" start server/app.js
