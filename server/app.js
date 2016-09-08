568e3b5ebdace481ebb9e5fbb6c547e33158bb24
root <aliriza.dikici@gmail.com>
2016-09-08 08:13:56 +0100

    Regular Commit

--- server/app.js
+++ server/app.js
@@ -32,7 +32,6 @@ db.on('close', function dbClosed(theResult){
 db.on('disconnect', function dbDisconnected(theResult){
 	console.log("=============================================================");
 	console.log("=============================================================");
-	console.log("=============================================================");
 	console.log("Database disconnected");
 	console.log(theResult);
 	console.log("=============================================================");
